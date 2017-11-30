55852520780a0024f8d4db1a8d00fd9db04bd2ce
Guy Pavlov <guy.pavlov84@gmail.com>
2017-11-29 17:49:35 -0800

    tight contour

--- image_diff.py
+++ image_diff.py
@@ -13,8 +13,23 @@ import numpy as np
 im1 = cv2.imread('im3.jpg')
 im2 = cv2.imread('im4.jpg')
 
+
 im1 = cv2.resize(im1, None, fx = 0.2, fy = 0.2, interpolation = cv2.INTER_CUBIC)
 im2 = cv2.resize(im2, None, fx = 0.2, fy = 0.2, interpolation = cv2.INTER_CUBIC)
+
+im1_cpy = im1.copy()
+im2_cpy = im2.copy()
+
+#im1 = cv2.Canny(im1, 10, 250)
+#cv2.imshow('im1', im1)
+#cv2.waitKey(0)
+
+#exit(0)
+
+
+
+
+
 
 im1 = cv2.fastNlMeansDenoisingColored(im1,None,50,50,7,21)
 im2 = cv2.fastNlMeansDenoisingColored(im2,None,50,50,7,21)
@@ -98,26 +113,42 @@ th3 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_
 #th3 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,11,2)
 
 thresh = th3
-cv2.imshow("Diff", diff)
-cv2.imshow("th3", th3)
 #cv2.imshow("denoised", denoised)
-cv2.waitKey(0)
+#cv2.waitKey(0)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9,9))
+dilated = cv2.dilate(thresh, kernel)
 
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
 # loop over the contours
-for c in cnts:
+for i, c in enumerate(cnts):
     # compute the bounding box of the contour and then draw the
     # bounding box on both input images to represent where the two
     # images differ
+
     (x, y, w, h) = cv2.boundingRect(c)
-    cv2.rectangle(im1, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    #cv2.rectangle(im1, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    #cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+mask = np.zeros_like(im1_cpy) # Create mask where white is what we want, black otherwise
+mask = cv2.drawContours(mask, cnts, -1, color=(255, 255, 255), thickness=cv2.FILLED) # Draw filled contour in mask
+out = np.zeros_like(im1_cpy) # Extract out the object and place into output image
+out[mask == 255] = im1_cpy[mask == 255]
+
+#cv2.imshow('mask', mask)
+cv2.imshow('out', out)
+
+#out = cv2.Canny(out, 10, 250)
+#cv2.imshow('out', out)
+
 
 # show the output images
-cv2.imshow("Original", im1)
-cv2.imshow("Modified", im2)
-cv2.imshow("Diff", diff)
+#cv2.imshow("Original", im1)
+#cv2.imshow("Modified", im2)
+#cv2.imshow("Diff", diff)
+#cv2.imshow("Contours", out)
 #cv2.imshow("Thresh", thresh)
+#cv2.imshow("Diff", diff)
+#cv2.imshow("th3", th3)
 cv2.waitKey(0)
